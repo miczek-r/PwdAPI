b4681a39ec8e5c144536850a68861555ecb9e692
Mateusz Błażków <matebla682@student.polsl.pl>
2021-04-13 16:25:54 +0200

    Added Home

---
+++ ZarzadzanieDomem/Models/Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZarzadzanieDomem.Models
+{
+    [Table("Homes")]
+    public class Home
+    {
+        [Key]
+        public int HomeId { get; set; }
+        public string HomeName { get; set; }
+        public string Adress { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
